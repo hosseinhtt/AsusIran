1a86a14d16edd51541febc077ebe8f29472199c5
hossein.htt <hossein.taybi@yahoo.com>
2023-10-03 10:21:22 +0330

    hierarchical/models.py modified

--- hierarchical/models.py
+++ hierarchical/models.py
@@ -5,6 +5,10 @@ class Category(MPTTModel):
     name = models.CharField(max_length=255)
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.CASCADE)
     
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
     class MPTTMeta:
         order_insertion_by = ['name']
 
