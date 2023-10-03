8b8c80b7f0cb3a38a0e9cd5755469dbd4bd58818
hossein.htt <hossein.taybi@yahoo.com>
2023-10-03 09:03:46 +0330

    all code modified

--- asus/urls.py
+++ asus/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('hierarchical/', include('hierarchical.urls')),
-    path('dynamic/', include('dynamic.urls')),
+    
 ]
--- hierarchical/views.py
+++ hierarchical/views.py
@@ -3,23 +3,60 @@ from .models import Category, Product
 
 def category_list(request):
     top_level_categories = Category.objects.filter(parent=None)
-    return render(request, 'hierarchical/category_list.html', {'categories': top_level_categories})
+    products = Product.objects.all()
+
+    context = {
+                'categories': top_level_categories,
+                'products': products
+               
+               }
+
+    return render(request, 'hierarchical/category_list.html', context)
 
 def category_detail(request, category_id):
     category = get_object_or_404(Category, pk=category_id)
-    # Get the hierarchical path to the selected category
-
+    
+    # Get the selected category and its descendants
+    categories = category.get_descendants(include_self=True)
+    
     category_path = category.get_ancestors(include_self=True)
 
-    products = Product.objects.filter(category=category)
+    # Get all products related to the selected categories
+    products = Product.objects.filter(category__in=categories)
+    
     subcategories = category.children.all()
-    context ={
-            'category': category,
-            'category_path': category_path,
-            'products': products,
-            'subcategories': subcategories,
-            }
+    
+    context = {
+        'category': category,
+        'category_path': category_path,
+        'products': products,
+        'subcategories': subcategories,
+    }
+    
     return render(request, 'hierarchical/category_detail.html', context)
+
+
+
+
+
+
+
+
+# def category_detail(request, category_id):
+#     category = get_object_or_404(Category, pk=category_id)
+#     # Get the hierarchical path to the selected category
+
+#     category_path = category.get_ancestors(include_self=True)
+
+#     products = Product.objects.filter(category=category)
+#     subcategories = category.children.all()
+#     context ={
+#             'category': category,
+#             'category_path': category_path,
+#             'products': products,
+#             'subcategories': subcategories,
+#             }
+#     return render(request, 'hierarchical/category_detail.html', context)
 
 
 def product_detail(request, product_id):
